063eacc0b9240159fa36c07c70f25e90d718f20e
KaidynH <kaidyn.haun@gmail.com>
2025-08-01 23:17:22 -0500

    Beat timing and nut collection

--- globals.py
+++ globals.py
@@ -2,6 +2,7 @@ import pygame
 from Sprites.baskets import basket
 from Sprites.squirrels import squirrel
 from Sprites.nuts import nut
+from Sprites.hitboxes import hitbox
 
 # Screen setup
 WIDTH = 600
@@ -17,12 +18,19 @@ FPS = 30
 VOLUME = 0.2
 
 # Sprites
-baskets = pygame.sprite.Group(
-    basket((WIDTH * 0.2, HEIGHT - 100)),
-    basket((WIDTH * 0.4, HEIGHT - 100)),
-    basket((WIDTH * 0.6, HEIGHT - 100)),
-    basket((WIDTH * 0.8, HEIGHT - 100))
-)
+f_basket = basket((WIDTH * 0.2, HEIGHT - 100))
+g_basket = basket((WIDTH * 0.4, HEIGHT - 100))
+h_basket = basket((WIDTH * 0.6, HEIGHT - 100))
+i_basket = basket((WIDTH * 0.8, HEIGHT - 100))
+
+f_hitbox = hitbox((f_basket.rect.centerx-15, f_basket.rect.centery-15))
+g_hitbox = hitbox((g_basket.rect.centerx-15, g_basket.rect.centery-15))
+h_hitbox = hitbox((h_basket.rect.centerx-15, h_basket.rect.centery-15))
+i_hitbox = hitbox((i_basket.rect.centerx-15, i_basket.rect.centery-15))
+
+baskets = pygame.sprite.Group(f_basket, g_basket, h_basket, i_basket)
+
+hitboxes = pygame.sprite.Group(f_hitbox, g_hitbox, h_hitbox, i_hitbox)
 
 squirrels = pygame.sprite.Group(
     squirrel((WIDTH * 0.2, 200)),
